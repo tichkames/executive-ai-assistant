0cc729c96c0126cecb8ad16494ff1e25f8a4186c
Tich <tich@exokames.com>
2025-11-20 17:37:33 +0200

    wip

--- eaia/reflection_graphs.py
+++ eaia/reflection_graphs.py
@@ -1,5 +1,4 @@
 from langgraph.store.base import BaseStore
-from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
 from typing import TypedDict, Optional
 from langgraph.graph import StateGraph, START, END, MessagesState
@@ -62,8 +61,10 @@ You should return the full prompt, so if there's anything from before that you w
 
 
 async def update_general(state: ReflectionState, config, store: BaseStore):
-    reflection_model = ChatOpenAI(model="o1", disable_streaming=True)
-    # reflection_model = ChatAnthropic(model="claude-3-5-sonnet-latest")
+    reflection_model = ChatAnthropic(
+        model="claude-sonnet-4-5-20250929",
+        disable_streaming=True
+    )
     namespace = (state["assistant_key"],)
     key = state["prompt_key"]
     result = await store.aget(namespace, key)
@@ -77,7 +78,7 @@ async def update_general(state: ReflectionState, config, store: BaseStore):
             instructions=instructions,
         )
         _output = await reflection_model.with_structured_output(
-            GeneralResponse, method="json_schema"
+            GeneralResponse
         ).ainvoke(prompt)
         return _output
 
@@ -148,7 +149,9 @@ class MultiMemoryInput(MessagesState):
 
 async def determine_what_to_update(state: MultiMemoryInput):
     reflection_model = ChatAnthropic(
-        model="claude-sonnet-4-5-20250929", disable_streaming=True)
+        model="claude-sonnet-4-5-20250929",
+        disable_streaming=True
+    )
     trajectory = get_trajectory_clean(state["messages"])
     types_of_prompts = "\n".join(
         [f"`{p_type}`: {MEMORY_TO_UPDATE[p_type]}" for p_type in state["prompt_types"]]
